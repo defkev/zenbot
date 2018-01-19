dc2107c49526a17001ff980bddff18e2e755d759
defkev <defkev@gmail.com>
2018-01-19 18:57:43 +0100

    Merge package.json/webpack.config.js with upstream/unstable

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules)/, query: { presets: ['es2015'] } },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules)/, query: { presets: ['env'] } },
       {
         test: /\.(scss)$/,
         use: [{
