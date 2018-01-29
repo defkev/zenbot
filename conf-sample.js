87fab09f1fc1a52b4c4bbc8c5a6d74ae58a9eaaa
defkev <defkev@gmail.com>
2018-01-30 00:48:40 +0100

    Merge conf-sample.js from upstream/unstable

--- conf-sample.js
+++ conf-sample.js
@@ -2,9 +2,14 @@ var c = module.exports = {}
 
 // mongo configuration
 c.mongo = {}
+c.mongo.db = 'zenbot4'
+
+// Must provide EITHER c.mongo.connectionString OR c.mongo.host,port,username,password
+// c.mongo.connectionString = 'mongodb://u:p@host/db?params'
+
+// The following is not needed when c.mongo.connectionString is provided:
 c.mongo.host = process.env.MONGODB_PORT_27017_TCP_ADDR || 'localhost'
 c.mongo.port = 27017
-c.mongo.db = 'zenbot4'
 c.mongo.username = null
 c.mongo.password = null
 // when using mongodb replication, i.e. when running a mongodb cluster, you can define your replication set here; when you are not using replication (most of the users), just set it to `null` (default).
@@ -141,6 +146,8 @@ c.post_only = true
 
 // default # days for backfill and sim commands
 c.days = 14
+// defaults to a high number of lookback periods
+c.keep_lookback_periods = 50000
 // ms to poll new trades at
 c.poll_trades = 30000
 // amount of currency to start simulations with
